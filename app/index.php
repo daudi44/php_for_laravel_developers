948a8d4ae00af171583f8defe49d5ba051b75e6e
daudi44 <daudi@iesebre.com>
2021-10-14 20:45:05 +0200

    Implementola funció greet que agafa l'informació dinàmica i retorna el missatge greet que volíem

--- app/index.php
+++ app/index.php
@@ -1,2 +1,17 @@
 <?php
-$greeting = 'Hello World';
+
+//Query String per agafar info dinàmica
+//var_dump ($_GET['name']);
+
+//API
+function greet(){
+    $name = htmlspecialchars($name = htmlspecialchars());
+    $cognom = htmlspecialchars($_GET['cognom']);
+
+    return "Hello ${name} ${cognom}!";
+}
+$greeting = greet();
+
+
+//Una altra manera de concatenar
+//$greeting = 'Hello ' . $_GET['name'] . ' ' . $_GET['cognom'] . '!';
